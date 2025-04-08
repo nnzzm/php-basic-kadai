f4d7a2b7f6c9e4080efe9b7e0c5cbf4e094a2ef0
nnzzm <nzmtomjerry1245@docomo.ne.jp>
2025-04-08 16:35:14 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  $associativeArray = [
+    "name" => "onion",
+    "price" => 200,
+    "weight" => 160,
+  ];
+  print_r($associativeArray);
+  ?>
+</body>
+</html>
