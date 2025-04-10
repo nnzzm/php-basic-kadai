57904f1d3fb0b5c690c5d4ad5dc627d48aafd977
nnzzm <nzmtomjerry1245@docomo.ne.jp>
2025-04-10 19:03:06 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  $array = ["名前"=>"玉ねぎ","値段"=>200,"産地"=>"北海道"];
+  foreach($array as $key => $value) {
+    echo $key . ":" . $value . "<br>";
+  }
+  ?>
+</body>
+</html>
